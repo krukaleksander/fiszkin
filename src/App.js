3dbd62925d64f5ac51d2c0d4c63f483f841b7625
Alek Kruk <kruk.aleksander@hotmail.com>
2020-01-21 06:48:14 +0100

    nav ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './styles/App.css';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,25 +10,42 @@ import {
 import Project from './components/Project';
 import Imex from './components/Imex';
 import StartLearn from './components/StartLearn';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import './styles/styles.css';
+window.jQuery = window.$ = $;
+require('bootstrap');
+
 
 function App() {
   return (
 
     <div className="App">
+      <header className="App-header">
+        <h1 className="main-heading">Fiszkin</h1>
+        <p className="main-desc">Darmowe narzędzie do nauki poprzez stosowanie fiszek =)</p>
+      </header>
       <Router>
         <div>
-          <ul>
-            <li>
-              <Link to="/start">Rozpocznij naukę</Link>
-            </li>
-            <li>
-              <Link to="/imex">Import / Export</Link>
-            </li>
-            <li>
-              <Link to="/project">O projekcie</Link>
-            </li>
-          </ul>
-
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/">Home /</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="main-nav navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/start">Rozpocznij naukę</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/imex">Import / Export</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/project">O projekcie</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
           <Switch>
             <Route path="/start">
               <StartLearn />
@@ -43,10 +59,7 @@ function App() {
           </Switch>
         </div>
       </Router>
-      <header className="App-header">
-        <h1 className="main-heading">Fiszkin</h1>
-        <p className="main-desc">Darmowe narzędzie do nauki poprzez stosowanie fiszek =)</p>
-      </header>
+
     </div>
   );
 }
