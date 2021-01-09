9a6d64b0e48ffed3cd2934c16bfee46804824c89
Alek Kruk <kruk.aleksander@hotmail.com>
2021-01-09 18:16:49 +0100

    create example of DB

---
+++ src/database/data.js
@@ -0,0 +1,37 @@
+const exampleBase = {
+    packageName: 'Animals',
+    creationDate: '2021-01-09',
+    content: [{
+            word: 'Cat',
+            translation: 'Kot',
+            remember: false
+        },
+        {
+            word: 'Dog',
+            translation: 'Pies',
+            remember: false
+        },
+        {
+            word: 'Parrot',
+            translation: 'Papuga',
+            remember: false
+        },
+        {
+            word: 'Hyena',
+            translation: 'Hiena',
+            remember: false
+        },
+        {
+            word: 'Lion',
+            translation: 'Lew',
+            remember: false
+        },
+        {
+            word: 'Mouse',
+            translation: 'Mysz',
+            remember: false
+        },
+    ]
+}
+
+export default exampleBase;
