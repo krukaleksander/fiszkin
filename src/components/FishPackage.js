2aabb402ca84a633fb0471f0862dc8b12ec8b7a8
Alek Kruk <kruk.aleksander@hotmail.com>
2021-01-11 18:31:14 +0100

    edit fish container done

--- src/components/FishPackage.js
+++ src/components/FishPackage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { VscChromeClose } from 'react-icons/vsc';
 import { AiFillEdit } from 'react-icons/ai';
 import { RiDeleteBinFill } from 'react-icons/ri';
@@ -6,14 +6,15 @@ import uniqid from 'uniqid';
 
 const FishPackage = ({ content, name, idOfPack, setShowDetails, data, setData }) => {
     const [fishes, setFishes] = useState(content);
+    const [editWord, setEditWord] = useState('parrot');
+    const [editTranslation, setEditTranslation] = useState('papuga');
+    const [showEditFish, setShowEditFish] = useState(false);
+
 
     const removeItem = (index) => {
-        console.log('im working');
         const newData = data.map((pack) => {
             console.log('generating newData');
             if (pack.packageId === idOfPack) {
-                console.log('id match')
-                console.log(pack);
                 const { packageName, creationDate, content } = pack;
                 const itemToRemove = content[index];
                 const newContent = content.filter((pair) => pair !== itemToRemove);
@@ -26,9 +27,7 @@ const FishPackage = ({ content, name, idOfPack, setShowDetails, data, setData })
                 }
 
             } else {
-                console.log('id do not match')
-                console.log(pack);
-                console.log(`Package id: ${pack.packageId}, id in state: ${idOfPack}`)
+
                 return pack;
             }
         });
@@ -36,12 +35,21 @@ const FishPackage = ({ content, name, idOfPack, setShowDetails, data, setData })
     };
 
     const editItem = (word, translation, id) => {
-        // function with is editing existing fish
+        // function with is editing existing fish       
+        // po kliknięciu w konkretną ikonkę generowany jest stan z word, translation i id
+        // pojawiają się input i text area, które są kontrolowane onChange z wartością aktualną. 
+        // jak delikwent kliknie w ikonkę że akceptuje to stan się czyści, i zmieniana jest wartośc konkretnej fiszki 
+        //odnalezionej po id
+        setShowEditFish(true);
+
     };
 
     const closeContainer = () => {
         setShowDetails(false);
     };
+    const closeEditFish = () => {
+        setShowEditFish(false);
+    }
     return (
         <div className="package-present">
             <VscChromeClose className='package-present__close' onClick={closeContainer} />
@@ -51,7 +59,7 @@ const FishPackage = ({ content, name, idOfPack, setShowDetails, data, setData })
                     const { word, translation } = fish;
                     return (
                         <div className="word" key={index}>
-                            <AiFillEdit className='word__edit' />
+                            <AiFillEdit className='word__edit' onClick={editItem} />
                             <div className='word__details'>
                                 <p className='word__name'>{word}</p>
                                 <div className="word__underline"></div>
@@ -63,6 +71,11 @@ const FishPackage = ({ content, name, idOfPack, setShowDetails, data, setData })
                     )
                 })}
             </div>
+            {showEditFish && <div className="edit-fish">
+                <VscChromeClose className='package-present__close' onClick={closeEditFish} />
+                <input className='edit-fish__input' type="text" value={editWord} onChange={(e) => setEditWord(e.target.value)} />
+                <textarea className='edit-fish__textarea' name="translation" id="" cols="30" rows="10" value={editTranslation} onChange={(e) => setEditTranslation(e.target.value)}></textarea>
+            </div>}
         </div>
     );
 }
