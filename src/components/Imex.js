6fc6e297e53fa7bd774a58e342a9bc6aaf14627b
Alek Kruk <kruk.aleksander@hotmail.com>
2021-01-17 08:20:57 +0100

    import page done

--- src/components/Imex.js
+++ src/components/Imex.js
@@ -1,8 +1,13 @@
 import React from 'react';
 const Imex = () => {
     return (
-        <div className="about">
-            <h1>Import/Export Twoich fiszek!</h1>
+        <div className="import">
+            <h1 className='import__heading'>Funkcja importu i exportu</h1>
+            <p className="import__par">Jest to następny etap projektu, zrealizowany będzie najpóźniej w marcu 2021 roku.</p>
+            <p className="import__par">Cała idea polega na tym, aby w ramach zmiany urządzenia, lub chęci korzystania z innej przeglądarki była możliwość przeniesienia swoich fiszek.</p>
+            <p className="import__par">W dalszym horyzoncie, jeżeli projekt w jakikolwiek sposób by się rozwijał - faktycznie znalazłby grono zadowolonych użytkowników - wprowadzone będzie po prostu zakładanie kont i problem zniknie.</p>
+            <p className="alert alert-success">Przypominam, że Fiszkin korzysta z pamięci w Twojej przeglądarce, dlatego musisz korzystać ciągle z tej samej</p>
+
         </div>
     );
 }
