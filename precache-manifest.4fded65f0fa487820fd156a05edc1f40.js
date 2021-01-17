2705f098b5751ade24fc4b2b49ce0cfe95813e7f
Alek Kruk <kruk.aleksander@hotmail.com>
2021-01-17 20:25:28 +0100

    Updates

---
+++ precache-manifest.4fded65f0fa487820fd156a05edc1f40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b2fd70a95dae57438147e7e45414cd",
+    "url": "/fiszkin/index.html"
+  },
+  {
+    "revision": "54805a0e0ee57d6bed4f",
+    "url": "/fiszkin/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "3d939fb7f2f0fc1bc9a0",
+    "url": "/fiszkin/static/css/main.927d7dfb.chunk.css"
+  },
+  {
+    "revision": "54805a0e0ee57d6bed4f",
+    "url": "/fiszkin/static/js/2.3ac311fa.chunk.js"
+  },
+  {
+    "revision": "e8648b194c47f9d57fb37ed45862c145",
+    "url": "/fiszkin/static/js/2.3ac311fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d939fb7f2f0fc1bc9a0",
+    "url": "/fiszkin/static/js/main.6ca95548.chunk.js"
+  },
+  {
+    "revision": "0bf67fe33285af6d72cd",
+    "url": "/fiszkin/static/js/runtime-main.cd37577d.js"
+  }
+]);
